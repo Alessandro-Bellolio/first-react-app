dff260f7d3da63f2ee9f3093edc2a7ad05aa4691
Alessandro <alessandro.bellolio@istea.com.ar>
2020-05-31 23:28:01 -0300

    Updates

---
+++ precache-manifest.cc90cc817e89128d6d953eb515182e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1f5c2b77a4ca63b39c99ccdd9d6351",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "615e9c895b4e26e6e47b",
+    "url": "/first-react-app/static/css/main.b0e1c6d6.chunk.css"
+  },
+  {
+    "revision": "6b7d4ddcbd5e57fc2708",
+    "url": "/first-react-app/static/js/2.848446d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app/static/js/2.848446d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615e9c895b4e26e6e47b",
+    "url": "/first-react-app/static/js/main.ecf8f6ca.chunk.js"
+  },
+  {
+    "revision": "32e1771c222861afd456",
+    "url": "/first-react-app/static/js/runtime-main.cb24fb53.js"
+  }
+]);
